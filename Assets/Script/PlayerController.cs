156d7bb3eb5095f18c91e6d5c1a9772979083a67
Rluan <jeong22437747@gmail.com>
2021-11-05 09:40:41 +0900

    cameraArm comment

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -107,9 +107,9 @@ public class PlayerController : MonoBehaviour
         TryJump();
         TryCrouch();
         Move();
-        CameraRotation();
+        //CameraRotation();
         CharacterRotation();
-        //LookAround();
+        LookAround();
         Interation();
         ItemSelect();
         Drop();
@@ -145,6 +145,22 @@ public class PlayerController : MonoBehaviour
         OneDown = Input.GetButtonDown("One");
         TwoDown = Input.GetButtonDown("Two");
     }
+   /* private void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        animator.SetBool("isMove", isMove);
+
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir; //moveDir
+            transform.position += moveDir * Time.deltaTime * 5f;
+        }
+    }*/
     private void Move()
     {
         if (!ischeck)
